5f03e088117445e9de5b1189a1bd9866b50f2354
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-02-15 02:30:18 -0800

    Porting to inquirer

--- index.js
+++ index.js
@@ -7,25 +7,40 @@ var Client = require('node-rest-client').Client;
 var program = require('commander');
 
 //require('./commands')(program);
+/**
+ * When example
+ */
 
+"use strict";
+var inquirer = require("inquirer");
+
+var questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your servicenow username",
+    },
+    {
+        type: "password",
+        message: "Enter your servicenow password",
+        name: "password"
+    }
+];
 
 program
     .command("get-hash")
     .description("Run this command to create/update the HASH used on webservice")
     .action(function () {
-        co(function *() {
-            var client = new Client();
+        var client = new Client();
 
-            var username = yield prompt('Username: ');
-            var password = yield prompt.password("Password: ");
-
+        inquirer.prompt(questions, function (answers) {
             var args = {
                 parameters: {
                     grant_type: "password",
                     client_id: "dd91abb1d06952001c186e0fc473f04f",
-                    //client_secret: "test",
-                    username: username,
-                    password: password
+                    client_secret: "PnJc[y+l5[",
+                    username: answers.username,
+                    password: answers.password
                 },
                 headers: {"Content-Type": "application/x-www-form-urlencoded"}
             };
@@ -35,8 +50,8 @@ program
             });
 
 
-            process.stdin.pause();
-        })();
+
+        });
     });
 
 
