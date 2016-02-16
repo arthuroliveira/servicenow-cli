5f7768f6665827b69ca20c8eca2adfe0c44f4484
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-02-15 16:13:08 -0800

    updated default config

--- commands/setup.js
+++ commands/setup.js
@@ -15,55 +15,46 @@ module.exports = function (program) {
             var config_path = path.join(process.cwd(), ".sn-config.json");
             var servicenow_config = default_config;
 
-            // Check if config file exist inside current folder
-            fs.lstat(config_path, function (err, stats) {
-                if (err) {
-
-                    console.log("Before we continue, please answer few questions");
-                    var questions = [
-                        {
-                            type: "input",
-                            name: "host",
-                            message: "Enter servicenow instance url",
-                        },
-                        {
-                            type: "input",
-                            name: "username",
-                            message: "Enter servicenow username",
-                        },
-                        {
-                            type: "password",
-                            message: "Enter your servicenow password",
-                            name: "password"
-                        },
-                        {
-                            type: "input",
-                            message: "RestAPI Client Id:",
-                            name: "client_id"
-                        },
-                        {
-                            type: "password",
-                            message: "RestAPI Client Secret:",
-                            name: "client_secret"
-                        }
-                    ];
-                    inquirer.prompt(questions, function (answers) {
-                        // If file doesnt exist, create one with default_config as content
-                        // update content based on answers
-                        fs.writeFile(config_path, JSON.stringify(default_config), function (err) {
-                            if (err) {
-                                return console.log(err);
-                            }
-                            console.log("The config file was created!");
-                            console.log("If you are using git, make sure to add .sn-config.json to gitignore");
-                        });
-                    });
-
-                } else {
-                    console.log('config file already exist!');
+            var questions = [
+                {
+                    type: "input",
+                    name: "host",
+                    message: "Enter servicenow instance url (https://\<instance\>.service-now.com). "
+                },
+                {
+                    type: "input",
+                    name: "proj_prefix",
+                    message: "Enter your project prefix (e.g. fidelity__). "
+                },
+                {
+                    type: "input",
+                    name: "username",
+                    message: "Enter servicenow username. "
+                },
+                {
+                    type: "password",
+                    message: "Enter your servicenow password. ",
+                    name: "password"
                 }
+            ];
+            inquirer.prompt(questions, function (answers) {
+                var hash = new Buffer(answers.username + ':' + answers.password).toString('base64');
+
+                servicenow_config.hash = hash;
+                servicenow_config.host = answers.host;
+                servicenow_config.project_prefix = answers.project_prefix;
+
+
+                fs.writeFile(config_path, JSON.stringify(servicenow_config), function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("The config file was created!");
+                    console.log("If you are using git, make sure to add .sn-config.json to gitignore");
+                });
             });
-            process.stdin.pause();
+
+            //process.stdin.pause();
         });
 
     return program;
