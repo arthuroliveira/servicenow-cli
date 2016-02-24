fa87b109cb28afcdbf5ed641ecfd330334ede4b5
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-02-24 15:34:02 -0800

    Giving the option to pull selected folder

--- commands/pull.js
+++ commands/pull.js
@@ -15,80 +15,94 @@ module.exports = function (program) {
         .description("Run this command to pull files from ServiceNow")
         .action(function () {
             require_config().then(function (config) {
+                service = ServiceNow(config);
+
                 var destination = path.join(process.cwd(), 'dist');
                 if (!fs.existsSync(destination)) {
                     fs.mkdirSync(destination);
                 }
 
-                service = ServiceNow(config);
 
-                for (folder in config.folders) {
-                    (function () {
+                var questions = [{
+                    type: "checkbox",
+                    name: "table",
+                    message: "What table(s) do you want to pull?",
+                    choices: Object.keys(config.folders)
+                },
+                    {
+                        type: "confirm",
+                        name: "specific_confirm",
+                        message: "Do you wanna pull a specific name?"
+                    },
+                    {
+                        type: "input",
+                        name: "specific",
+                        message: "What filename do you want to pull?",
+                        when: function (answers) {
+                            return answers.specific_confirm;
+                        }
+                    }
+                ];
 
-                        var fname = folder;
-                        var folder_path = path.join(destination, folder);
+                inquirer.prompt(questions, function (answers) {
 
-                        fs.mkdir(folder_path, function (err) {
-                            if (err) {
-                                if (err.code != 'EEXIST') {
-                                    console.log(err); // something else went wrong
-                                    return;
-                                }
-                            }
+                    for (key in answers.table) {
+
+                        var folder = answers.table[key];
 
 
-                            var tableName = config.folders[fname].table;
-                            service.tableName = tableName;
+                        (function () {
 
-                            service.getRecords({
-                                query: config.folders[fname].key + "STARTSWITH" + config.project_prefix,
-                                row: 50
-                            }, function (err, data) {
+                            var fname = folder;
+                            var folder_path = path.join(destination, folder);
+
+                            fs.mkdir(folder_path, function (err) {
                                 if (err) {
-                                    console.log(err);
-                                } else {
-                                    data.records.forEach(function (element, index) {
-                                        var content = element[config.folders[fname].field];
-                                        var filename = element.name + "." + config.folders[fname].extension;
-                                        var file_path = path.join(folder_path, filename);
-
-                                        fs.writeFile(file_path, content, function (err) {
-                                            if (err) {
-                                                console.log("ERR", err);
-                                            } else {
-                                                console.log("Touching file " + file_path);
-                                            }
-                                        });
-                                    });
+                                    if (err.code != 'EEXIST') {
+                                        console.log(err); // something else went wrong
+                                        return;
+                                    }
                                 }
+
+
+                                var tableName = config.folders[fname].table;
+                                service.tableName = tableName;
+                                var query;
+                                if (answers.specific) {
+                                    query = config.folders[fname].key + "STARTSWITH" + answers.specific;
+                                } else {
+                                    query = config.folders[fname].key + "STARTSWITH" + config.project_prefix;
+                                }
+
+                                service.getRecords({
+                                    query: query,
+                                    rows: 50
+                                }, function (err, data) {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        data.records.forEach(function (element, index) {
+                                            var content = element[config.folders[fname].field];
+                                            var filename = element.name;
+                                            var filenameWithExtension = filename + "." + config.folders[fname].extension;
+                                            var file_path = path.join(folder_path, filenameWithExtension);
+
+                                            fs.writeFile(file_path, content, function (err) {
+                                                if (err) {
+                                                    console.log("ERR", err);
+                                                } else {
+                                                    console.log("Touching file " + file_path);
+                                                }
+                                            });
+                                        });
+                                    }
+                                });
+
+
                             });
-
-
-                        });
-                    })();
-                }
-
-
-                //var questions = [
-                //    {
-                //        type: "input",
-                //        name: "table",
-                //        message: "What table would you like to fetch? ",
-                //        default: "incident"
-                //    }];
-                //
-                //inquirer.prompt(questions, function (answers) {
-                //    var service = ServiceNow(answers.table, config);
-                //
-                //
-                //    service.getRecords({rows: 10, name: "fidelity__homepage"}, function (err, obj) {
-                //        if (err) {
-                //            console.log("ERROR " + err);
-                //        }
-                //
-                //        console.log(JSON.stringify(obj));
-                //    });
-                //});
+                        })();
+                    }
+                });
 
             });
         });
